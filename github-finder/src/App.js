5a4e6fab203337b65d4e7a37d28f6ee7e405abcd
Jackslaps <jackslaps@pop-os.localdomain>
2019-07-11 22:46:33 -0400

    Added navbar component, default props and prop types

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -4,8 +4,12 @@ import Navbar from './components/layout/NavBar'
 
 class App extends Component {
     render() {
+        //const numbers = [1,2,3,4,5];
+
         return (
             <div className='App'>
+                {/* <Navbar title={numbers}/>*/} {/*This will create an error in the browser's console because the prop "title" is an array, */}
+                                                 {/* but the requirement for the prop in Navbar.js is for strings only */}
                 <Navbar />
             </div>
         )
--- github-finder/src/components/layout/NavBar.js
+++ github-finder/src/components/layout/NavBar.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
+
 
 export class NavBar extends Component {
+	static defaultProps = {
+		title: 'Github Finder',
+		icon: 'fab fa-github'
+	};
+	// You can set static default props if there arent any special classNames being passed down from App.js.
+	// If someone passes props down through <Navbar /> in App.js, the default props will get overwritten.
+
+	static propTypes = {
+		title: PropTypes.string.isRequired,
+		icon: PropTypes.string.isRequired
+	}
   render() {
     return (
-        <nav className='navbar bg-primary'>
-            <h1>Navbar</h1>
+        <nav className="navbar bg-primary">
+            <h1>
+							<i className={ this.props.icon } /> { this.props.title }
+            </h1>
         </nav>
-    ) ;
+    );
   }
 }
 
