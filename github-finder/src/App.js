3efc09bd7890f2548166c50d3be79c3185be4a96
Jackslaps <jackslaps@pop-os.localdomain>
2019-07-20 23:20:30 -0400

    Added lifecycle methods, added api calls to populate site

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/layout/NavBar'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/NavBar";
 //import UserItem from './components/users/UserItem'
-import Users from './components/users/Users';
+import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
-    render() {
-        //const numbers = [1,2,3,4,5];
+  state = {
+    users: [],
+    loading: false
+  };
 
-        return (
-            <div className='App'>
-                {/* <Navbar title={numbers}/>*/} {/*This will create an error in the browser's console because the prop "title" is an array, */}
-                                                 {/* but the requirement for the prop in Navbar.js is for strings only */}
-                <Navbar />
-                <div className="container">
-                    <Users />  
-                </div>
-                
-                {/* <UserItem /> */}
-            </div>
-        )
-    }
+  async componentDidMount() {
+		this.setState({ loading: true });
+
+		const res = await axios.get("https://api.github.com/users");
+		
+		this.setState({ users: res.data, loading: false })
+    console.log(res.data);
+  }
+
+  render() {
+    //const numbers = [1,2,3,4,5];
+
+    return (
+      <div className="App">
+        {/* <Navbar title={numbers}/>*/}{" "}
+        {/*This will create an error in the browser's console because the prop "title" is an array, */}
+        {/* but the requirement for the prop in Navbar.js is for strings only */}
+        <Navbar />
+        <div className="container">
+          <Users loading={ this.state.loading } users={ this.state.users }/>
+        </div>
+        {/* <UserItem /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
