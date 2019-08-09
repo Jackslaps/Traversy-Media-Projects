d7f0ba462b70b9d0cd397cc557631c9e7f638758
Jackslaps <jackslaps@pop-os.localdomain>
2019-08-08 23:20:53 -0400

    Added Context, implemented it in App.js as well as creating githubContext.js, Reducer, and State

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -10,7 +10,10 @@ import axios from "axios";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 
+import GithubState from "./context/github/GithubState";
+
 const App = () => {
+  // This replaces the state object below
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
@@ -102,51 +105,54 @@ const App = () => {
   // const { users, user, repos, loading } = this.state;
 
   return (
-    <Router>
-      <div className="App">
-        {/* <Navbar title={numbers}/>*/}{" "}
-        {/*This will create an error in the browser's console because the prop "title" is an array, */}
-        {/* but the requirement for the prop in Navbar.js is for strings only */}
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <Fragment>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={showAlert}
+    <GithubState>
+      <Router>
+        <div className="App">
+          {/* <Navbar title={numbers}/>*/}{" "}
+          {/*This will create an error in the browser's console because the prop "title" is an array, */}
+          {/* but the requirement for the prop in Navbar.js is for strings only */}
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={searchUsers}
+                      clearUsers={clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={showAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={getUser}
+                    getUserRepos={getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
                   />
-                  <Users loading={loading} users={users} />
-                </Fragment>
-              )}
-            />
-            <Route exact path="/about" component={About} />
-            <Route
-              exact
-              path="/user/:login"
-              render={props => (
-                <User
-                  {...props}
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos}
-                  loading={loading}
-                />
-              )}
-            />
-          </Switch>
+                )}
+              />
+            </Switch>
+          </div>
+          {/* <UserItem /> */}
         </div>
-        {/* <UserItem /> */}
-      </div>
-    </Router>
+      </Router>
+    </GithubState>
   );
 };
 
 export default App;
+
