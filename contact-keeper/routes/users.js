7e8ce2476e26f1fb4fa51697fe4bbaedde408d86
Jackslaps <crodr521@gmail.com>
2019-08-25 23:06:31 -0400

    Added User schema, created validation on Users routes

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/Users");
 
 // @route   POST api/users
 // @desc    This will register a user
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Register a user')
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add name")
+      .not()
+      .isEmpty(),
+      check('email', 'please include a valid email').isEmail(),
+      check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    res.send('Passed');
+  }
+);
 
 module.exports = router;
